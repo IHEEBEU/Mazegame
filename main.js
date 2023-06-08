7dc084c7175fa87fbe3ed8a57fd16738413f8f07
Fareszran <frareszran11@gmail.com>
2023-06-08 11:26:43 +0100

    done

--- main.js
+++ main.js
@@ -2,6 +2,153 @@
 /* ALOUI HERE */
 
 
+
+/* FARES HERE */
+function displaymaze(){
+    $("#maze").css("display","block")
+}
+
+
+/* IHEB HERE */
+//References
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+//Questions and Options array
+
+const quizArray = [
+    {
+        id: "0",
+        question: "What were some of the major developments during the Neolithic Age?",
+        options: ["Inovation of the wheel and writing systems", "Domestication of animals and practice of agriculture", "Exploration of new continents and sea routes", "Invention of advanced metalworking techniques"],
+        correct:"Domestication of animals and practice of agriculture",
+    },
+    {
+        id: "1",
+        question: "What materials were commonly used to make tools during the Neolithic Age?",
+        options: ["Stone,bone and wood", "Bronze and iron", "Copper and gold", "Clay and pottery"],
+        correct: "Stone,bone and wood",
+    },
+    {
+        id: "2",
+        question: "How did trade contribute to the growth of Neolithic societies?",
+        options: ["It led to the establishment of permanent settlements", "It facilitated cultural exchanges and the spread of ideas", "It resulted in the invention of new tools and technologies", "It allowed for the discovery of new land resources"],
+        correct: "It facilitated cultural exchanges and the spread of ideas",
+    },
+    {
+        id: "3",
+        question: "What social changes occurred during the Neolithic Age?",
+        options: ["The rise of writing systems and complex legal codes", "The emergence of democratic systems and universal suffrage", "The development of social hierachies and leadership roles", "the adoption of monotheistic religions and organized priesthoods"],
+        correct:  "The development of social hierachies and leadership roles",
+    },
+    
+];
+
+//Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+
+//Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        //increment questionCount
+        questionCount += 1;
+        //if last question
+        if (questionCount == quizArray.length) {
+            //hide question container and display score
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            //user score
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            //display questionCount
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + quizArray.length + " Question";
+            //display quiz
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+    })
+);
+
+//Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count == 0) {
+            clearInterval(countdown);
+            displayNext();
+        }
+    }, 1000);
+};
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation
+function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+        //randomly sort options
+        i.options.sort(() => Math.random() - 0.5);
+        //quiz card creation
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        //question number
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+        //question
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        //options
+        div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+        quizContainer.appendChild(div);
+    }
+}
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+
     //if user clicked answer == correct option stored in object
     if (userSolution === quizArray[questionCount].correct) {
         userOption.classList.add("correct");
@@ -23,7 +170,6 @@
         element.disabled = true;
     });
 }
-/* FARES HERE */
 
 //initial setup
 function initial() {
